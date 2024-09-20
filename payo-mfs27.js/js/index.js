049e7950088fff0cfaa2571ec0e0729767488d3f
unknown <khadijasheikh266@gmail.com>
2024-09-20 11:13:11 +0600

    added simple login handler and prevent default loading

---
+++ payo-mfs27.js/js/index.js
@@ -0,0 +1,11 @@
+// search: form submit reloading the page
+// step:1 set event hanlder
+document.getElementById('button-login').addEventListener('click',function(event){
+    // step:2 prevent default behavior(prevent reloading browser)
+    event.preventDefault();
+    console.log('login button is clicked')
+    // step:3 get the phone number
+    const phoneNumber=document.getElementById('phone-number').value;
+    console.log(phoneNumber)
+
+})
